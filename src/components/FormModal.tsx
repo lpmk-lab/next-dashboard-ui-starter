45ca5ba29c998b287696b81ba1f1183bc2be02ba
lapyaeminkhant.sbs <lapyaeminkhant.sbs@gmail.com>
2024-12-15 20:59:15 +0630

    merge

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -62,7 +62,8 @@ const FormModal: React.FC<FormModalProps> = ({ table, type, data, id }) => {
         />
       </button>
       {open && (
-        <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+        <div className="fixed inset-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          {/* <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center"></div> */}
           <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] ">
             <Form />
             <div
